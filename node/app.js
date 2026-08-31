033e6e71b088352864bc52519f2dcad7aed94777
Varun <varungautam100@gmail.com>
2026-08-31 11:56:42 +0545

    Modify the app.js file

--- node/app.js
+++ node/app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("This is a node app. The change has been reflected");
+  res.end("This is a node app. The change has been reflected again");
 });
 
 server.listen(PORT, HOST, () => {
