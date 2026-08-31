e80563c684ca97142634c6313fbdbcf038c2161f
Varun <varungautam100@gmail.com>
2026-08-31 22:19:12 +0545

    Check the flow

--- node/app.js
+++ node/app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("This is a node app guys...... The change has been reflected again back");
+  res.end("This is a node app guyzz. The change has been reflected again back");
 });
 
 server.listen(PORT, HOST, () => {
